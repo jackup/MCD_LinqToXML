4c3dc192415d6af0194b7c80605a7bd12b1a1aed
jackup <yk.58@hotmail.com>
2021-12-22 17:41:15 +0300

    Add project files.

---
+++ MCD_LinqToXML/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MCD_LinqToXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xDoc = new XDocument(
+                new XDeclaration("1.0", "UTF-8", "yes"),
+                new XComment("Yorum Satırı"),
+                new XElement("Personellerim",
+                new XElement("Personel", new XAttribute("ID", "1"),
+                new XElement("İsim", "Ahmet"),
+                new XElement("Soyisim","Koç"),
+                new XElement("EmailAdres", "ahmet.koc@gmail.com")
+                ),
+                new XElement("Personel", new XAttribute("ID", "2"),
+                new XElement("İsim", "Mehmet"),
+                new XElement("Soyisim", "Dana"),
+                new XElement("EmailAdres", "mehmet.dana@gmail.com")
+                )
+                )
+                );
+            xDoc.Save("c:\\XML\\Ornek1.xml");
+        }
+    }
+}
